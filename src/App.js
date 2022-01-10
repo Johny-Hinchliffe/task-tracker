26bef16dc732643c4cb6b78a6f3fb3996cbead7c
Johny-Hinchliffe <82289369+Johny-Hinchliffe@users.noreply.github.com>
2022-01-10 08:54:43 +0000

    nearly finished the task tracker, just need to add a few more bits

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 
 function App() {
 	const [tasks, setTasks] = useState([
@@ -36,16 +37,34 @@ function App() {
 		},
 	])
 
+	//Add Task
+	const addTask = (task) => {
+		const id = Math.floor(Math.random() * 100000000) + 1
+		const newTask = { id, ...task }
+		setTasks([...tasks, newTask])
+	}
+
 	//Delete Task List
 	const deleteTask = (id) => {
 		setTasks(tasks.filter((task) => task.id !== id))
 	}
 
+	// Toggle reminder
+
+	const toggleReminder = (id) => {
+		setTasks(
+			tasks.map((task) =>
+				task.id === id ? { ...task, reminder: !task.reminder } : task
+			)
+		)
+	}
+
 	return (
 		<div className="container">
 			<Header />
+			<AddTask onAdd={addTask} />
 			{tasks.length > 0 ? (
-				<Tasks tasks={tasks} onDelete={deleteTask} />
+				<Tasks tasks={tasks} onToggle={toggleReminder} onDelete={deleteTask} />
 			) : (
 				'Add a Task 😁'
 			)}
